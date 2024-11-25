91f55fb4d56fceea981746dba64cb618186e9d11
zw <1657129601@qq.com>
2024-11-25 11:35:41 +0800

    update

---
+++ cfg.py
@@ -0,0 +1,89 @@
+config = {
+    # 'BJN260_aug': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\BJN260_full_resolution/',
+    #             'data_lst': 'BJN260_crack_five.txt'},
+
+    'Deepcrack':{'data_root': '/home/pb/zw/datasets/DeepCrack/',
+                'data_lst': 'deep_all_train_four.txt'},
+    # 'Deepcrack':{'data_root': '/media/pb/sdb2/zw2024/dataset/DeepCrack_stable/',
+    #             'data_lst': 'train_stable_deepcrack_16.txt'},
+    # 'ZDcrack':{'data_root': 'E:\zw2023\datasets\zd_dataset\data/',
+    #             'data_lst': 'zd_crack_fusion.txt'},
+
+    # 'Crack500_aug': {'data_root': 'E:\zw2023\datasets\pavement crack datasets\CRACK500/',
+    #                 'data_lst': 'train.txt'},
+
+    # 'Crack360_aug': {'data_root': 'Crack360/train/',
+    #                 'data_lst': 'Crack360_aug_train_pair.lst'},
+
+    # 'Rain365_aug': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\Rain365_full_resolution/',
+    #                 'data_lst': 'Rain365_crack_five.txt'},
+
+    # 'BRSfusion_aug': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\BRS_fusion/',
+    #                 'data_lst': 'BRS_crack_fusion_five.txt'},
+
+    # 'Sun520_aug': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\Sun520_full_resolution/',
+    #                 'data_lst': 'Sun520_crack_five.txt'}
+    # 'BJN260_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/BJN260_full_resolution/',
+    #             'data_lst': 'BJN260_crack_four.txt'},
+
+    # 'BJN260_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/BJN260_full_resolution/',
+    #             'data_lst': 'BJN260_train_img_fusion_orignal.txt'},#完整分辨率
+
+
+    'BJN260_aug': {'data_root': '/media/pb/sdb2/zw2024/dataset/BJN260/',
+                'data_lst': 'BJN260_crack_four_orignal.txt'},#完整分辨率4倍
+
+    # 'BJN260_aug': {'data_root': '/media/pb/sdb2/zw2024/dataset/BJN260/',
+    #             'data_lst': 'BJN260_train_img_stable.txt'},#stable diffusion增强
+    # 'Crack360_aug': {'data_root': 'Crack360/train/',
+    #                 'data_lst': 'Crack360_aug_train_pair.lst'},
+
+    'Rain365_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/Rain365_full_resolution/',
+                    'data_lst': 'Rain365_crack_four.txt'},
+    'BRSfusion_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/BRS_fusion/',
+                    'data_lst': 'BRS_crack_fusion_four.txt'},
+
+    # 'Sun520_aug': {'data_root': 'Sun520/train/',
+    #                 'data_lst': 'Sun520_aug_train_pair.lst'}
+    'Sun520_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/Sun520_full_resolution/',
+                    'data_lst': 'Sun520_crack_four.txt'}
+    
+}
+
+config_test = {
+    # 'BJN260_aug': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\BJN260_full_resolution/test/test_img/',
+    #             'data_lst': 'BJN260_crack_test.txt'},
+
+    'Deepcrack': {'data_root': '/home/pb/zw/datasets/DeepCrack/test_img/',
+                'data_lst': 'deepcrack_test.txt'},
+
+    # 'BRSfusion_aug': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\BRS_fusion/',
+    #             'data_lst': 'deepcrack_test.txt'},
+
+    # 'ZDcrack': {'data_root': 'E:\zw2023\datasets\zd_dataset\data/test/image/',
+    #             'data_lst': 'ZDcrack_test.txt'},
+    # 'Crack500_aug': {'data_root': 'E:\zw2023\datasets\pavement crack datasets\CRACK500/testcrop/',
+    #                 'data_lst': 'test.txt'},
+
+    # 'Crack360': {'data_root': 'Crack360/test/img/',
+    #                   'data_lst': 'CRKWH100.lst'},
+
+    # 'Rain365': {'data_root':'E:\zw2023\datasets\crack_datasets\crack\Rain365_full_resolution/test\img/',
+    #                   'data_lst': 'Rain365_crack_test.txt'},
+
+    # 'Sun520': {'data_root': 'E:\zw2023\datasets\crack_datasets\crack\Sun520_full_resolution/test\img/',
+    #                   'data_lst': 'Sun520_crack_test.txt'}
+    # 'BJN260_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/BJN260_full_resolution/test/test_img/',
+    #             'data_lst': 'BJN260_crack_test.txt'},
+    'BJN260_aug': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/BJN260_full_resolution/test/test_img_orignal/',
+                'data_lst': 'BJN260_crack_test.txt'},#完整分辨率
+
+    # 'Crack360': {'data_root': 'Crack360/test/img/',
+    #                   'data_lst': 'CRKWH100.lst'},
+
+    'Rain365': {'data_root':'/home/pb/zw/datasets/crack_datasets/crack/Rain365_full_resolution/test/img/',
+                      'data_lst': 'Rain365_crack_test.txt'},
+
+    'Sun520': {'data_root': '/home/pb/zw/datasets/crack_datasets/crack/Sun520_full_resolution/test/img/',
+                      'data_lst': 'Sun520_crack_test.txt'}
+}
